ffcb3ad56cbfa1340464ff7d711759a9bb6a1f3c
meav <milanistavietnam@gmail.com>
2020-05-01 22:29:22 +0700

    Add files via upload

---
+++ javascript/scroll_in_navbar.js
@@ -0,0 +1,9 @@
+$('.nav li a').on('click', function(){
+if(!$( this ).hasClass('dropdown-toggle')){
+$('.navbar-collapse').collapse('hide');
+event.preventDefault();
+$('html, body').animate({ scrollTop: 0 });
+}
+else {$('.nav').animate({ scrollTop: $("#scroll-item").offset().top }, 1500);
+}
+});
