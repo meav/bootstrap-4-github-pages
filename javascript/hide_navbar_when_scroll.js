c14bf323573a6a20e8c542d3c7cfd9a1c0d35893
meav <milanistavietnam@gmail.com>
2020-05-01 22:47:08 +0700

    Update hide_navbar_when_scroll.js

--- javascript/hide_navbar_when_scroll.js
+++ javascript/hide_navbar_when_scroll.js
@@ -8,3 +8,13 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 };
+
+$('.nav li a').on('click', function(){
+if(!$( this ).hasClass('dropdown-toggle')){
+$('.navbar-collapse').collapse('hide');
+event.preventDefault();
+$('html, body').animate({ scrollTop: 0 });
+}
+else {$('.nav').animate({ scrollTop: $("#scroll-item").offset().top }, 1500);
+}
+})
