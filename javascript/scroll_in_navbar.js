7b3c32c535923862ecc011fabdc420081c555156
meav <milanistavietnam@gmail.com>
2020-05-01 22:56:10 +0700

    Delete scroll_in_navbar.js

--- javascript/scroll_in_navbar.js
+++
@@ -1,9 +0,0 @@
-$('.nav li a').on('click', function(){
-if(!$( this ).hasClass('dropdown-toggle')){
-$('.navbar-collapse').collapse('hide');
-event.preventDefault();
-$('html, body').animate({ scrollTop: 0 });
-}
-else {$('.nav').animate({ scrollTop: $("#scroll-item").offset().top }, 1500);
-}
-})
